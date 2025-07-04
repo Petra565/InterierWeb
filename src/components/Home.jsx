4ca7591d16457da7f2f1fb3b2dbb96060b7dd2a4
Petra565 <sim.petra01@gmail.com>
2025-07-04 14:11:44 +0200

    pridanie popisu + responzivita

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,13 @@
                                    md:h-56 md:w-56
                                       xl:h-64 xl:w-64"
                     src="../../images/logo.jpg"></img>
-                <div className="flex h-full flex-col gap-4 px-4 pb-10">
-                    <div className="place-items-center rounded-xl border-1 border-white bg-black/50 p-4 text-white
-                                    sm:absolute sm:top-[45%] sm:left-1/20 sm:w-4/10
+                <div className="flex h-full flex-col gap-6 pb-10">
+                    <div className="mx-4 place-items-center rounded-xl border-1 border-white bg-black/50 p-4 text-white
+                                    sm:absolute sm:top-[35%] sm:left-1/20 sm:w-4/10
+                                      md:top-[35%]
                                        lg:top-1/2 lg:w-3/10 lg:-translate-y-1/2 lg:transform">
                         <ul className="text-md list-inside list-disc leading-6 subpixel-antialiased
-                                          sm:text-xl
-                                                    md:leading-8
+                                          sm:text-xl sm:leading-8
                                                         lg:text-2xl lg:leading-10">
                             <li>nábytok na mieru</li>
                             <li>kuchynské linky</li>
@@ -22,11 +22,13 @@
                             <li>detské izby</li>
                         </ul>
                     </div>
-                    <div className="place-items-center rounded-xl border-1 border-white bg-black/50 p-4 text-white
-                                    sm:absolute sm:top-[45%] sm:right-1/20 sm:w-4/10
+
+                    <div className="mx-4 place-items-center rounded-xl border-1 border-white bg-black/50 p-4 text-white
+                                    sm:absolute sm:top-[35%] sm:right-1/20 sm:w-4/10
+                                      md:top-[35%]
                                         lg:top-1/2 lg:w-3/10 lg:-translate-y-1/2 lg:transform">
                         <ul className="text-md list-inside list-disc leading-6 subpixel-antialiased
-                                        sm:text-xl
+                                        sm:text-xl sm:leading-8
                                             md:leading-8
                                                 lg:text-2xl lg:leading-10">
                             <li>podlahy</li>
@@ -36,7 +38,17 @@
                             <li>montáž dverí</li>
                         </ul>
                     </div>
+                    <p className=" mb-4 w-full bg-black/50 p-4 text-justify text-white sm:absolute sm:bottom-[10%] md:bottom-[8%] md:p-6 md:text-lg lg:p-6 xl:bottom-[6%] xl:left-1/2 xl:w-[88%] xl:-translate-x-1/2 xl:transform xl:rounded-xl xl:border xl:border-white xl:text-xl 2xl:bottom-[8%]">
+                        Nábytok na mieru je to, čo doladí vaše vysnívané bývanie do dokonalosti.
+                        Či už ide o vstavané skrine, malé skrinky pod umývadlo v kúpelni alebo komplikovanú kuchyňu, v ktorej bude radosť variť a samozrejme,
+                        aj radosť sa na ňu pozerať.
+                        Nezabúdame na oddychovú zónu, ktorej neodmysliteľnou súčasťou je obývacia stena, ani na pracovnú zónu, kde komfort zabezpečí ideálny pracovný stôl.
+                        Skrátka, predstavivosti sa medze nekladú.
+                        Skúsený stolár sa svojím precíznym prístupom postará nielen o kvalitný nábytok, ale vďaka dlhoročnej praxi aj o servis okien.
+                    </p>
                 </div>
+
+               
             </div>
         </>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,17 +9,17 @@ function NavBar() {
         <>
             <nav className="flex h-15 w-full place-items-center bg-black text-white/100 sm:bg-black/50">
                 <ul className="grid w-full grid-cols-12 px-1 text-center">
-                    <li className="col-span-4 sm:col-span-2 lg:col-span-1">
+                    <li className="col-span-4 sm:col-span-2 xl:col-span-1">
                         <Link
                             to="/"
                             className={`block w-full rounded px-6 py-2 text-center transition hover:bg-white/50 hover:text-black${currentPath === "/" ? "bg-white border-1 border-white" : ""}`}
                         >
-                            Domov
+                            O nás
                         </Link>
                     </li>
 
                     <li
-                        className="col-span-4 sm:col-span-2 lg:col-span-1">
+                        className="col-span-4 sm:col-span-2 xl:col-span-1">
                         <Link
                             to="/gallery"
                             className={`text-centertransition block w-full rounded px-6 py-2 hover:bg-white/50 hover:text-black${currentPath === "/gallery" ? "bg-white border-1 border-white" : ""}`}
@@ -29,7 +29,7 @@ function NavBar() {
                     </li>
 
                     <li
-                        className="col-span-4 sm:col-span-2 lg:col-span-1">
+                        className="col-span-4 sm:col-span-2 xl:col-span-1">
                         <Link
                             to="/contact"
                             className={`block w-full rounded px-6 py-2 text-center transition hover:bg-white/50 hover:text-black${currentPath === "/contact" ? "bg-white border-1 border-white" : ""}`}
